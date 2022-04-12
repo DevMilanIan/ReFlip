ca25240be3100b1065d2faf2e60a8b37f3483f49
DevMilanIan <milanian775@gmail.com>
2022-04-12 17:01:53 -0400

    update-server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 app.use('/images', express.static('images'));
@@ -10,4 +11,4 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
 });
 
-app.listen(17000, () => console.log("Server running..."));
+app.listen(port, () => console.log("Server running on port: " + port));
