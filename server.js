2907b04451efb7626a6c1681db23713514ea3f04
devmilanian <milanian775@gmail.com>
2022-06-12 17:01:06 -0400

    Merge branch 'main' of https://git.heroku.com/reflip

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
-app.use('/images', express.static('images'));
+app.use("/static", express.static(path.resolve(__dirname, "static")));
+app.use('/images', express.static(path.resolve(__dirname, "images")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
+    res.sendFile(path.resolve(__dirname, "index.html"))
 });
 
 app.listen(port, () => console.log("Server running on port: " + port));
